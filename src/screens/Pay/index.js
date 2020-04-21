62420afd5dfdfa9a1b4dc35fe48b5b2cf35b6885
mfzaguiar <first_mf@hotmail.com>
2020-04-21 18:39:30 -0300

    add bottom tabs navigation and styles

--- src/screens/Pay/index.js
+++ src/screens/Pay/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-export default function Home() {
-  return <Text>Home</Text>;
+export default function Pay() {
+  return <Text>Pay</Text>;
 }
