8cdc64ed18fd2725063336f1436747671697965e
mfzaguiar <first_mf@hotmail.com>
2020-04-27 17:46:31 -0300

    add Banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 import * as S from './styles';
 
-const Banner = () => <S.Container></S.Container>;
+import img13 from '../../../assets/images/13.png';
 
-export default Banner;
+export default function Banner() {
+  return (
+    <S.Container>
+      <S.Details>
+        <S.Title>Cobre um amigo</S.Title>
+
+        <S.Description>
+          Mantenha suas parcerias em dia, use o PicPay para fazer suas cobranças
+        </S.Description>
+      </S.Details>
+      <S.Img source={img13} />
+    </S.Container>
+  );
+}
